e832c24ba18c7fe2806e3a733dbdb3c4837ad3bb
Antonio A. Alves <antonioaugusto.alves1981@gmail.com>
2023-09-21 21:13:50 -0300

    Criação da tela da calculadora

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author antonio
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        FrmCalculadora calc = new FrmCalculadora();
+        calc.setTitle("Calculator Tabajara");
+        calc.setLocationRelativeTo(calc);
+        calc.setVisible(true);
+    }
+    
+}
